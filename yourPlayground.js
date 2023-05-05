515c81522e2672d005a8071e7d0bddd66a37f61d
ThanOs <lads42279@gmail.com>
2023-05-06 05:02:10 +0530

    initial commit

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,3 +1,27 @@
+
+
+
+// let numbers = [2,5,4]
+// for (number of numbers ){
+
+//     console.log(number)
+
+// }
+// console.log(numbers)
+
+
+
+//functions 
+
+// function consol() {
+//     console.log("im logging on console")
+// }
+// function sum(){
+//     return 0
+// }
+// consol()
+// console.log(sum())
+
 //Objects
 
 // const indentifier = (name, shirt, assets, liability) => {
@@ -31,8 +55,8 @@
 
 
 // const double = (numbers) =>{
-    
-    
+
+
 //     let result = []
 //     for (const number of numbers) {
 //         result.push(number * 2);
@@ -42,14 +66,141 @@
 
 // console.log(double([2,3,4,5]))
 
-const square = (numbers) =>{
-    
-    
-    let result = []
-    for (const number of numbers) {
-        result.push(number * number);
-    }
-    return result
-}
+// const square = (numbers) =>{
 
-console.log(square([2,3,4,5]))
+
+//     let result = []
+//     for (const number of numbers) {
+//         result.push(number * number);
+//     }
+//     return result
+// }
+
+// console.log(square([2,3,4,5]))
+
+
+//letter counter (loop)
+// let result = 0
+// const letter_counter = () => {
+//     const phase = 'hey, im the line'
+//     for(const letter in phase){
+//         console.log(Number(letter)+1);
+//         result = Number(letter)+1
+//     }
+//     return {result} 
+// }
+// console.log(letter_counter())
+
+// const phrase = "Its like your my favourite incomplete wish 🥰"
+// const howManyLetters = (phrase) => {
+//     let result = 0;
+//     for (const letter of phrase) {
+//         console.log((letter) + 1)
+//         result = Number(letter) + 1
+//     }
+
+//     return { result }
+// }
+
+// console.log(howManyLetters(phrase));
+
+
+
+// const sum = (nums) => {
+
+//     let result = 0;
+
+//     for (numbers of nums) {
+//         console.log(numbers);
+//         result = result + numbers;
+//     }
+//     return { result }
+
+// }
+// console.log(sum(nums))
+
+
+
+
+//finding max
+// const nums = [1, 2, 3, 4, 20]
+
+// const max = (nums) => {
+//     let max =0;
+//     for(number of nums){
+//         if(number > max){
+//             max = Number(number);
+//         }
+//     }
+//     return {max}
+
+
+// }
+// console.log(max(nums))
+
+
+
+//letter frequency
+
+
+// const phrase = "Its like your my favourite incomplete wish wish wish 🥰"
+
+// const letter_frequency = (phrase) => {
+//     frequency ={}
+
+//     for (letter of phrase) {
+//       if(letter in frequency){
+//         frequency[letter]= frequency[letter]+1;
+//       }
+//       else{
+//         frequency[letter] = 1;
+//       }
+//     }
+//     console.log(letter);
+//     return frequency 
+// }
+
+// console.log(letter_frequency(phrase))
+// const phrase = "fuck you bitch fuck you bitch"
+
+// const wordFrequency = (phrase) => {
+//     let frequency = {};
+
+//     words = phrase.split(' ')
+//     for (const word of words) {
+//         if (word in frequency) {
+//             frequency[word] += 1;
+//         }
+//         else {
+//             frequency[word] = 1;
+//         }
+//     }
+//     return frequency
+// }
+// console.log(wordFrequency(phrase))
+
+//Math Method
+
+// const double = (numbers) =>{
+
+//     return numbers.map(number => number *2)
+
+
+// }
+// console.log(double([2,4,5,6]))
+numbers=[4,5,6,4,5,55,99]
+// const greater = (numbers, greaterThan)=>{
+// let result = []
+
+// for(const number of numbers){
+//     if(number>greaterThan){
+//         result.push(number);
+//     }
+// }
+
+// return result
+
+
+// }
+// console.log(greater([1,2,3,4,5],2))
+console.log(numbers.filter(nums => nums > 21 && nums < 90));
